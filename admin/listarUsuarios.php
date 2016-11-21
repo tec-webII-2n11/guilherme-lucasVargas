26844ef6eeb8cc6a696cd9b5a58ebfd5c0f319fa
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2016-11-21 00:07:41 +0000

    final commit

--- admin/listarUsuarios.php
+++ admin/listarUsuarios.php
@@ -5,6 +5,21 @@
     <?php 
     $active = "usuarios";
     include("../admin/include/header.php"); 
+    include ("../include/conecta_mysql.php");
+    $tabela = "<table>
+                    <th>Nome</th>
+                    <th>email</th>
+                    ";
+    
+    $conecta = mysqli_query($conexao, "SELECT * FROM usuarios");
+    while ($row = mysqli_fetch_array($conecta)){
+        $tabela .= "<tr>
+                    <td>".$row["nome"]."</td>
+                    <td>".$row["email"]."</td>
+                    </tr>";
+    }
+    
     ?>
 </body>
+    <?=$tabela?>
 </html>
